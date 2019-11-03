ed1da62cb0a4ac4788864964fd3d804387884c79
Shubham Karala <shubham@ramanujan.du.ac.in>
2019-11-03 04:27:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,23 @@
-class Song
-  attr_accessor :title, :artist
+class Song 
+  attr_accessor :name, :artist 
 
-  def initialize(title)
-    @title = title
+  @@all = []
+
+  def initialize(name, artist=nil)
+    @name = name
+    @artist = artist
+    @@all << self
   end
 
-  def artist=(artist)
-    @artist = self
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+
+  def self.all
+    @@all
   end
 end
